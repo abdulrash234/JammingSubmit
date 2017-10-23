a1ac4f69b65318c35d8520cbd8d783aeaf5b3bb3
Abdulrashid Mohammed <abdulrash234@yahoo.com>
2017-10-23 12:13:01 +0200

    first Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+   <h1>Ja<span class="highlight">mmm</span>ing</h1>
+   <div class="App">
+     <!-- Add a SearchBar component -->
+     <div class="App-playlist">
+       <!-- Add a SearchResults component -->
+       <!-- Add a Playlist component -->
+     </div>
+   </div>
+ </div>
+    );
+  }
+}
+
+export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
